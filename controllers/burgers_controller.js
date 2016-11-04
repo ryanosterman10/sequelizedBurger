f98f76efdeacd53831b162e66277c56cd9db236f
Ryan Osterman <ryan_osterman@med.unc.edu>
2016-11-04 13:27:05 -0400

    got rid of connection and orm files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get('/burgers', function(req, res){
 });
 
 router.post('/burgers/create', function(req, res){
-	burgers.create(['burger_name'], [req.body.b_name], function(data){
+	burger.create({ burger_name: req.body.burger_name }, { devoured: req.body.devoured } function(data){
 		res.redirect('/burgers')
 	});
 });
@@ -25,17 +25,9 @@ router.post('/burgers/create', function(req, res){
 router.put('/burgers/update/:id', function(req, res){
 	var condition = 'id = ' + req.params.id;
 	console.log('condition ', condition);
-	burgers.update({'devoured': req.body.devoured}, condition, function(data){
+	burger.update({'devoured': req.body.devoured}, condition, function(data){
 		res.redirect('/burgers');
 	});
-});
-
-router.delete('/burgers/delete/:id', function (req, res) {
-  var condition = 'id = ' + req.params.id;
-  console.log('condition',condition);
-  burger.delete(condition, function () {
-    res.redirect('/burgers');
-  });
 });
 
 module.exports = router;
