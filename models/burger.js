847a08389e7ec2b6e26b0a9a6a60d2f6c224c9c1
Ryan Osterman <ryan_osterman@med.unc.edu>
2016-11-04 09:51:06 -0400

    initial commit

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require('../config/orm.js');
+
+var burgers = {
+	all: function(cb){
+		orm.all('burgers', function(res){
+			cb(res);
+		});
+	},
+	create: function(col, vals, cb){
+		orm.create('burgers', col, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb){
+		orm.update('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	delete: function (condition, cb){
+		orm.delete('burgers', condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports = burgers;
